1696facb96822d212ed85f36773bdbd3e8e7d5c8
BirMohandAmokrane <birmoho@gmail.com>
2026-02-14 15:48:55 +0100

    ticket-1 : page index

--- app/index.php
+++ app/index.php
@@ -1,6 +1,15 @@
 <?php
 // Créer l'objet $pdo en récupérant les infos de connexion à la BDD dans le docker_compose
+$pdo = new \PDO('mysql:host=mysql;dbname=courses;charset=utf8mb4','user','pwd');
+
 // Récupérer dans la BDD tous les produits. Vérifier avec un var_dump($products)
+$sql = "SELECT * FROM product ";
+$request = $pdo->prepare($sql);
+$request->execute();
+$products = $request->fetchAll(PDO::FETCH_ASSOC);
+
+//var_dump($products[0]);
+
 // Afficher tous les produits dans le HTML ci-dessous avec un foreach
 
 ?>
@@ -10,6 +19,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index-style.css">
     <title>Liste de courses</title>
 </head>
 <body>
@@ -20,11 +30,13 @@
         <input type="submit" value="Ajouter">
     </form>
     <ul>
+        <? foreach ($products as $product ) {?> 
         <li>
-            Tomate
+            <div class="product-name"><?php echo htmlspecialchars($product['name']) ?></div>
             <a href="">❌</a>      
             <a href="">✏️</a>
         </li>
+        <?php } ?>
     </ul>
 
     <script>
